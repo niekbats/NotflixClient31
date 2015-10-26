0d762ef95016a64681f19bddfa5486be40a7a9ef
DoronHartog <doronhartog@Hotmail.com>
2015-10-26 20:46:05 +0100

    ajax frontpage

--- NotflixClient31.js
+++ NotflixClient31.js
@@ -1,3 +1,31 @@
-function main() {
-    
-}
+$( document ).ready(function() {
+  console.log( "document loaded" );
+
+	$.ajax({ 
+	     type: "GET",
+	     dataType: "json",
+	     url: "http://localhost:8080/notflix31/api/movies",
+
+		     	success: function(data){        
+		       	console.log(data);
+
+					$.each(data, function(index, element) {
+					 	++index;
+					 	$('.headerMovie'+index).html(element.titel);
+					 	$('.descriptionMovie'+index).html(element.beschrijving);
+
+					 		$.ajax({ 
+					     type: "GET",
+					     dataType: "json",
+					     url: "http://www.omdbapi.com/?t=" + element.titel + "&y=&plot=short&r=json",
+
+							    success: function(data){       
+
+							      document.getElementById("imageMovie"+index).src = data.Poster;
+						    	}
+					  });
+        	});
+
+		     }
+	  });
+});
