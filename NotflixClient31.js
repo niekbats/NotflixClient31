6965ff08365b5f9196a4ba8c1889a2a37c0842b0
DoronHartog <doronhartog@Hotmail.com>
2015-10-28 11:43:34 +0100

    movies and users

--- NotflixClient31.js
+++ NotflixClient31.js
@@ -1,34 +1,134 @@
 $( document ).ready(function() {
   console.log( "document loaded" );
+});
 
+function getFrontPageMovies(){
 	$.ajax({ 
+	 type: "GET",
+	 dataType: "json",
+	 url: "http://localhost:8080/notflix31/api/movies",
+
+	     	success: function(data){        
+	       	console.log(data);
+
+				$.each(data, function(index, element) {
+				 	++index;
+				 	if(index == 4){
+				 		return;
+				 	}
+				 	$('.headerMovie'+index).html(element.titel);
+				 	$('.descriptionMovie'+index).html(element.beschrijving);
+
+				 		$.ajax({ 
+				     type: "GET",
+				     dataType: "json",
+				     url: "http://www.omdbapi.com/?t=" + element.titel + "&y=&plot=short&r=json",
+
+						    success: function(data){       
+
+						      document.getElementById("imageMovie"+index).src = data.Poster;
+					    	}
+				  	});
+			});
+
+	     }
+	});
+}
+
+
+function showMovieDetails() {
+	var titel = getName();
+	console.log("titel: "+titel);
+	var newWindow = window.open("details.html");
+}
+
+function loadDetails(){
+	var titel = getName();
+	console.log("titel: "+titel);
+
+		$.ajax({ 
 	     type: "GET",
 	     dataType: "json",
-	     url: "http://localhost:8080/notflix31/api/movies",
+	     url: "http://localhost:8080/notflix31/api/movies/titel?titel="+titel,
+
+		     	success: function(data){        
+		       	console.log(data);
+
+				 	$('#detailsTitel').html(data.titel);
+				 	$('#detailsBeschrijving').html("<strong>Description:</strong><br>" + data.beschrijving);
+				 	$('#detailsRegiseur').html("Regiseur: "+ data.regiseur);
+				 	$('#detailsImdbCode').html("Imdb Code: "+data.imdbcode);
+				 	$('#detailsLengteMin').html("Lengte in minuten: "+data.lengteMin);
+				 	$('#detailsPublicatieDatum').html("Publicatie datum: "+data.publicatieDatum);
+				 	$('#detailsAvarageNumber').html("Imdb rating: "+data.avarageNumber);
+
+				 		$.ajax({ 
+				     type: "GET",
+				     dataType: "json",
+				     url: "http://www.omdbapi.com/?t=" + titel + "&y=&plot=short&r=json",
+
+						    success: function(data){       
+
+						      document.getElementById("detailsImageMovie").src = data.Poster;
+					   }
+				});
+	     	}
+	  });
+}
+
+function showAllMovies() {
+	$.ajax({ 
+		 type: "GET",
+		 dataType: "json",
+		 url: "http://localhost:8080/notflix31/api/movies",
 
 		     	success: function(data){        
 		       	console.log(data);
 
 					$.each(data, function(index, element) {
 					 	++index;
-					 	$('.headerMovie'+index).html(element.titel);
-					 	$('.descriptionMovie'+index).html(element.beschrijving);
-
 					 		$.ajax({ 
 					     type: "GET",
 					     dataType: "json",
 					     url: "http://www.omdbapi.com/?t=" + element.titel + "&y=&plot=short&r=json",
 
 							    success: function(data){       
-
-							      document.getElementById("imageMovie"+index).src = data.Poster;
+							    	$("#movieContainer").append('<div class="col-md-4"><img id="imageMovie'+index+'" src="" alt="movie" width="275" height="430"><h2 height="200" id="headerMovie'+index+'">Spotlight movie number 1</h2><p height="300" id="descriptionMovie'+index+'">Information about the movie</p><p><a class="btn btn-default" href="details.html" role="button">View details »</a></p></div>');
+							      	document.getElementById("imageMovie"+index).src = data.Poster;
+							      	$('#headerMovie'+index).html(element.titel);
+					 				$('#descriptionMovie'+index).html(element.beschrijving);
 						    	}
-					  });
-        	});
+					  	});
+				});
 
 		     }
-	  });
-});
+		});
+}
+
+function showAllUsers() {
+	$.ajax({ 
+		 type: "GET",
+		 dataType: "json",
+		 url: "http://localhost:8080/notflix31/api/users",
+
+		     	success: function(data){        
+		       	console.log(data);
+
+					$.each(data, function(index, element) {
+					 	++index;
+    
+							    	$("#userContainer").append('<div class="col-md-4"><h2 id="headerUser'+index+'">nickname</h2><p id="voornaamUser'+index+'">voornaam</p><p id="tussenvoegselUser'+index+'">tussenvoegsel</p><p id="achternaamUser'+index+'">achternaam</p><a class="btn btn-default" href="details.html" role="button">View details »</a></p></div>');
+							      	$('#headerUser'+index).html(element.nickname);
+					 				$('#voornaamUser'+index).html("Voornaam: "+element.voornaam);
+					 				$('#tussenvoegselUser'+index).html("Tussenvoegsel: "+element.tussenvoegsel);
+					 				$('#achternaamUser'+index).html("Achternaam: "+element.achternaam);
+						    	
+				});
+
+		     }
+		});
+}
+
 
 function login() {
 	$.ajax({
@@ -41,8 +141,6 @@ function login() {
 	}).done(function(data) {  	
   	//Do something with the data
 		alert("succesvol ingelogt");
-   		});
-})
-  });
+   	});
 	return false;
 }
